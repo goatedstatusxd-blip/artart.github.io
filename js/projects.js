838cbad58bbbcc72d2d351b4ccc1738090b704c7
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 22:09:58 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,10 @@
 const projects = [
   // Videos
-  { title: "Kur Sea Vai", type: "video", thumbnail: "assets/images/kur sea vai/kadrai1.png", link: "assets/videos/video1.mp4" },
-  { title: "Radio", type: "video", thumbnail: "assets/images/radio/radio1.png", link: "assets/videos/video2.mp4" },
+  { title: "Kur Sea Vai", type: "video", thumbnail: "assets/images/kur sea vai/kadrai1.png", link: "https://www.youtube.com/embed/ol1oq-GpWec" },
+  { title: "Radio", type: "video", thumbnail: "assets/images/radio/radio1.png", link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview" },
 
   // Photography
-  { title: "Antradienis", type: "photo", thumbnail: "assets/images/antradienis/antradienis1.png", link: "assets/images/photo1.jpg" },
+  { title: "Antradienis", type: "photo", thumbnail: "assets/images/antradienis/antradienis1.png", link: "assets/images/antradienis/antradienis1.png" },
   { title: "Photo Series 2", type: "photo", thumbnail: "assets/images/photo2.jpg", link: "assets/images/photo2.jpg" },
 
   // Drawings & Paintings
