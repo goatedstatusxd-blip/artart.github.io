d37d14f4da6f90e7abe583fe5934c57ecba92776
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 21:20:30 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,11 @@
 const projects = [
   {
     title: "Kur Sea Vai",
-    image: "assets/images/project1.jpg",
+    image: "assets/images/kadrai1.png",
     link: "project1.html"
   },
   {
-    title: "Another Work",
+    title: "Radio  ",
     image: "assets/images/project2.jpg",
     link: "project2.html"
   }
