a30aae483f0598312de4a7a26ec0c99c63e3fa16
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 21:11:10 +0100

    Create projects.js

---
+++ js/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+  {
+    title: "Kur Sea Vai",
+    image: "assets/images/example.jpg",
+    link: "project.html?project=kur-sea-vai"
+  },
+  {
+    title: "Another Project",
+    image: "assets/images/example2.jpg",
+    link: "project.html?project=another"
+  }
+];
