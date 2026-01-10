b3abd2b9ece21dd331ad3c82b08cfeb8780e3ca1
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 22:40:13 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,9 @@
 const projects = [
-  // Videos
   { title: "Kur Sea Vai", type: "video", thumbnail: "assets/images/kur sea vai/kadrai1.png", link: "https://www.youtube.com/embed/ol1oq-GpWec" },
   { title: "Radio", type: "video", thumbnail: "assets/images/radio/radio1.png", link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview" },
-
-  // Photography
   { title: "Antradienis", type: "photo", thumbnail: "assets/images/antradienis/antradienis1.png", link: "assets/images/antradienis/antradienis1.png" },
   { title: "Laukiniai", type: "photo", thumbnail: "assets/images/laukiniai/laukiniai1.png", link: "assets/images/laukiniai/laukiniai1.png" },
-
-  // Drawings & Paintings
   { title: "Sketch 1", type: "drawing", thumbnail: "assets/images/drawing1.jpg", link: "assets/images/drawing1.jpg" },
   { title: "Painting 1", type: "drawing", thumbnail: "assets/images/drawing2.jpg", link: "assets/images/drawing2.jpg" },
+  // add more projects here
 ];
