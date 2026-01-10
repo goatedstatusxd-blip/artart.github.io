d493954b7e854ea2022621aa011e3196a77267df
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 22:52:13 +0100

    Create project-data.js

---
+++ js/project-data.js
@@ -0,0 +1,36 @@
+// Add description and optional gallery
+const projects = [
+  {
+    title: "Kur Sea Vai",
+    type: "video",
+    thumbnail: "assets/images/kur sea vai/kadrai1.png",
+    link: "https://www.youtube.com/embed/ol1oq-GpWec",
+    description: "Directed and produced video about sea adventures.",
+    gallery: [
+      { type: "video", link: "https://www.youtube.com/embed/ol1oq-GpWec" },
+      { type: "photo", link: "assets/images/kur sea vai/kadrai2.png" },
+      { type: "photo", link: "assets/images/kur sea vai/kadrai3.png" }
+    ]
+  },
+  {
+    title: "Radio",
+    type: "video",
+    thumbnail: "assets/images/radio/radio1.png",
+    link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview",
+    description: "Short film produced for radio project.",
+    gallery: [
+      { type: "video", link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview" }
+    ]
+  },
+  {
+    title: "Antradienis",
+    type: "photo",
+    thumbnail: "assets/images/antradienis/antradienis1.png",
+    link: "assets/images/antradienis/antradienis1.png",
+    description: "Photography series exploring everyday life.",
+    gallery: [
+      { type: "photo", link: "assets/images/antradienis/antradienis2.png" },
+      { type: "photo", link: "assets/images/antradienis/antradienis3.png" }
+    ]
+  }
+];
