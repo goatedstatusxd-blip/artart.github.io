efa392371e81de2cd385f53739183046f594e344
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-10 21:26:04 +0100

    Update image paths for project assets

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,12 @@
 const projects = [
   {
     title: "Kur Sea Vai",
-    image: "assets/images/kadrai1.png",
+    image: "assets/images/kadrai/kadrai1.png",
     link: "project1.html"
   },
   {
     title: "Radio",
-    image: "assets/images/radio1.png",
+    image: "assets/images/radio/radio1.png",
     link: "project2.html"
   }
 ];
