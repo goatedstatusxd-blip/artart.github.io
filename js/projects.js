530a3cd86cb549267e96d2e78e8aa397d6e7ec5d
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-11 01:46:00 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -4,32 +4,33 @@ const projects = [
     type: "video",
     thumbnail: "assets/images/kur sea vai/kadrai1.png",
     link: "https://www.youtube.com/embed/ol1oq-GpWec",
-    gallery: [ // gallery images only
+    description: "Short film directed by Aura Vilkisiute.",
+    gallery: [
       "assets/images/kur sea vai/kadrai1.png",
-      "assets/images/kur sea vai/kadrai2.png"
-    ],
-    description: "Directed short film about..."
+      "assets/images/kur sea vai/kadrai2.png",
+      "assets/images/kur sea vai/kadrai3.png"
+    ]
   },
   {
     title: "Radio",
     type: "video",
     thumbnail: "assets/images/radio/radio1.png",
     link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview",
+    description: "Experimental video project.",
     gallery: [
       "assets/images/radio/radio1.png",
       "assets/images/radio/radio2.png"
-    ],
-    description: "Experimental radio video..."
+    ]
   },
   {
     title: "Antradienis",
     type: "photo",
     thumbnail: "assets/images/antradienis/antradienis1.png",
     link: "assets/images/antradienis/antradienis1.png",
+    description: "Photography series.",
     gallery: [
       "assets/images/antradienis/antradienis1.png",
       "assets/images/antradienis/antradienis2.png"
-    ],
-    description: "Photography series..."
+    ]
   }
 ];
