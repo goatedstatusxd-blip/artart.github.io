23cc0ee5978b6927a8b916b440e9ddbac2a74226
goatedstatusxd-blip <goatedstatusxd@gmail.com>
2026-01-11 01:44:04 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,35 @@
 const projects = [
-  // Videos
-  { title: "Kur Sea Vai", type: "video", thumbnail: "assets/images/kur sea vai/kadrai1.png", link: "https://www.youtube.com/embed/ol1oq-GpWec" },
-  { title: "Radio", type: "video", thumbnail: "assets/images/radio/radio1.png", link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview" },
-
-  // Photography
-  { title: "Antradienis", type: "photo", thumbnail: "assets/images/antradienis/antradienis1.png", link: "assets/images/antradienis/antradienis1.png" },
-  { title: "Laukiniai", type: "photo", thumbnail: "assets/images/laukiniai/laukiniai1.png", link: "assets/images/laukiniai/laukiniai1.png" },
-
-  // Drawings & Paintings
-  { title: "Sketch 1", type: "drawing", thumbnail: "assets/images/drawing1.jpg", link: "assets/images/drawing1.jpg" },
-  { title: "Painting 1", type: "drawing", thumbnail: "assets/images/drawing2.jpg", link: "assets/images/drawing2.jpg" },
+  {
+    title: "Kur Sea Vai",
+    type: "video",
+    thumbnail: "assets/images/kur sea vai/kadrai1.png",
+    link: "https://www.youtube.com/embed/ol1oq-GpWec",
+    gallery: [ // gallery images only
+      "assets/images/kur sea vai/kadrai1.png",
+      "assets/images/kur sea vai/kadrai2.png"
+    ],
+    description: "Directed short film about..."
+  },
+  {
+    title: "Radio",
+    type: "video",
+    thumbnail: "assets/images/radio/radio1.png",
+    link: "https://drive.google.com/file/d/1EPTAeF77ZEEp9xZeQ5n1I69FFpjbLRLr/preview",
+    gallery: [
+      "assets/images/radio/radio1.png",
+      "assets/images/radio/radio2.png"
+    ],
+    description: "Experimental radio video..."
+  },
+  {
+    title: "Antradienis",
+    type: "photo",
+    thumbnail: "assets/images/antradienis/antradienis1.png",
+    link: "assets/images/antradienis/antradienis1.png",
+    gallery: [
+      "assets/images/antradienis/antradienis1.png",
+      "assets/images/antradienis/antradienis2.png"
+    ],
+    description: "Photography series..."
+  }
 ];
